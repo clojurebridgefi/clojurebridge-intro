e41105dd2350162d1df1d208ee3aa0f0b311f0f3
Jarppe <jarppe@metosin.fi>
2018-02-13 12:29:43 +0200

    Upgraded some deps

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject clojurebridge-intro "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [quil "2.5.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [quil "2.6.0"]])
